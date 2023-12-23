93b1974073b12131b3752561fa308a47f849a3d9
Hidayathulla Shaik <hidayathullashaik@Hidayathullas-MacBook-Pro-2.local>
2023-12-23 08:21:50 +0530

    Functional Interfaces

---
+++ src/functionalinterfaces/InterfaceStaticMethod.java
@@ -0,0 +1,7 @@
+package functionalinterfaces;
+
+public interface InterfaceStaticMethod {
+    static void staticMeth() {
+        System.out.println("This is a static method in interface");
+    }
+}
---
+++ src/functionalinterfaces/InterfaceStaticMethodImpl.java
@@ -0,0 +1,7 @@
+package functionalinterfaces;
+
+public class InterfaceStaticMethodImpl implements InterfaceStaticMethod {
+    public static void main(String[] args) {
+        InterfaceStaticMethod.staticMeth();
+    }
+}
